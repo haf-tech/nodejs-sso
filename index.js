a5b8231f1cd0e524fdb6d2f94a155d776da7672f
Hafid Haddouti <code@haddouti.com>
2021-07-04 20:22:43 +0200

    Update docu and logic to support ApiStrategy and calling remote services with access token

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ const axios = require('axios');
 // OpenID, WebApp support
 const passport = require('passport');
 const WebAppStrategy = require('ibmcloud-appid').WebAppStrategy;
+const APIStrategy = require("ibmcloud-appid").APIStrategy;
+const TokenManager = require('ibmcloud-appid').TokenManager;
+const ApiStrategy = require('ibmcloud-appid/lib/strategies/api-strategy');
  
 
 // *** Prometheus 
@@ -65,6 +68,22 @@ const LOGIN_ANON_URL = "/ibm/appid/loginanon";
 const ROP_LOGIN_PAGE_URL = "/ibm/appid/rop/login";
 
 
+// Retrieve the OpenID/OAuth information from the env
+const ssoProviderUrl = process.env.OAUTH_URL;
+const ssoSecret = process.env.CLIENT_SECRET;
+const ssoClientId = process.env.CLIENT_ID;
+const ssoTenantId = process.env.TENANT_ID;
+const sspAppBaseUrl = process.env.REDIRECT_URL;
+
+const config = {
+	tenantId: ssoTenantId,
+	clientId: ssoClientId,
+	secret: ssoSecret,
+	oauthServerUrl: ssoProviderUrl,
+  redirectUri: sspAppBaseUrl + CALLBACK_URL
+};
+
+
 // Setup express application to use express-session middleware
 // Must be configured with proper session storage for production
 // environments. See https://github.com/expressjs/session for
@@ -81,33 +100,31 @@ app.use(express.static(__dirname ));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Retrieve the OpenID/OAuth information from the env
-const ssoProviderUrl = process.env.OAUTH_URL;
-const ssoSecret = process.env.CLIENT_SECRET;
-const ssoClientId = process.env.CLIENT_ID;
-const ssoTenantId = process.env.TENANT_ID;
-const sspAppBaseUrl = process.env.REDIRECT_URL;
-
-passport.use(new WebAppStrategy({
-  tenantId: ssoTenantId,
-  clientId: ssoClientId,
-  secret: ssoSecret,
-  oauthServerUrl: ssoProviderUrl,
-  redirectUri: sspAppBaseUrl + CALLBACK_URL
-}));
+passport.use(new WebAppStrategy(config));
+passport.use(new ApiStrategy(config));
+const tokenManager = new TokenManager(config);
 
 // Configure passportjs with user serialization/deserialization. This is required
 // for authenticated session persistence across HTTP requests. See passportjs docs
 // for additional information http://passportjs.org/docs
+
 passport.serializeUser(function(user, cb) {
-  cb(null, user);
+  console.log('serializeUser()', user);
+  if(user == null) {
+    cb(null, '0');
+  } else {
+    cb(null, user);
+  }
 });
 
 passport.deserializeUser(function(obj, cb) {
   cb(null, obj);
 });
 
-// Explicit login endpoint. Will always redirect browser to login widget due to {forceLogin: true}. If forceLogin is set to false the redirect to login widget will not occur if user is already authenticated
+
+
+// Explicit login endpoint. Will always redirect browser to login widget due to {forceLogin: true}. 
+// If forceLogin is set to false the redirect to login widget will not occur if user is already authenticated
 app.get(LOGIN_URL, passport.authenticate(WebAppStrategy.STRATEGY_NAME, {
   successRedirect: LANDING_PAGE_URL,
   forceLogin: true
@@ -129,7 +146,25 @@ app.get(LOGOUT_URL, function(req, res){
 // Protected area. If current user is not authenticated - redirect to the login widget will be returned.
 // In case user is authenticated - a page with current user information will be returned.
 app.get("/protected", passport.authenticate(WebAppStrategy.STRATEGY_NAME), function(req, res){
+  
+  var idpAuthContext = req.session[WebAppStrategy.AUTH_CONTEXT];
+  // ATTENTION: Print out the Access/Identity Token Payload
+  console.log('AccessTokenPayload:', idpAuthContext.accessTokenPayload);
+  console.log('IdentityTokenPayload:', idpAuthContext.identityTokenPayload);
+
   res.json(req.user);
+});
+
+// Protected area. If current user is not authenticated - redirect to the login widget will be returned.
+// In case user is authenticated - a page with current user information will be returned.
+app.get("/api/protected", passport.authenticate(APIStrategy.STRATEGY_NAME), function(req, res){
+  
+  var idpAuthContext = req.appIdAuthorizationContext;
+  // ATTENTION: Print out the Access/Identity Token Payload
+  console.log('AccessTokenPayload:', idpAuthContext.accessTokenPayload);
+  console.log('IdentityTokenPayload:', idpAuthContext.identityTokenPayload);
+
+  res.json(idpAuthContext.accessTokenPayload);
 });
 
 app.post("/rop/login/submit", bodyParser.urlencoded({extended: false}), passport.authenticate(WebAppStrategy.STRATEGY_NAME, {
@@ -147,6 +182,42 @@ app.get(ROP_LOGIN_PAGE_URL, function(req, res) {
 // ############# Utilities
 const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
 
+async function getAppIdentityToken() {
+	try {
+			const tokenResponse = await tokenManager.getApplicationIdentityToken();
+      return tokenResponse;
+	} catch (err) {
+			console.log('err obtained : ' + err);
+	}
+}
+
+httpBackendResponses = '';
+function httpRequest(url, accessToken, customHeaderValue, serviceName) {
+
+  const req = axios.get(url, {
+    headers: {      
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,     
+        'X-Custom': customHeaderValue 
+    }
+  })
+  .then(function (response) {
+    console.log(`Secure Backend "${serviceName}" done`, response.status);
+    httpBackendResponses += '<br />' + 'Secure Backend "secured/date": ' +  JSON.stringify(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(`Secure Backend "${serviceName}" done`, error.message);
+    httpBackendResponses += '<br />' + `Secure Backend "${serviceName}": Communication Failed: ${error.message}`;
+  })
+  .finally(function () {
+
+    console.log(`Secure Backend "${serviceName}" done.`);
+  }); 
+
+  return req;
+}
+
 // ############# Entry points
 app.get('/', (req, res) => {
   
@@ -160,11 +231,39 @@ app.get('/', (req, res) => {
     res.send(ret);    
 });
 
+// ############# Call (secured) backend service
+app.get('/backend', passport.authenticate(WebAppStrategy.STRATEGY_NAME, {
+  successRedirect: '/secure/backend',
+  forceLogin: false
+}));
+
+app.get('/secure/backend', passport.authenticate(WebAppStrategy.STRATEGY_NAME), function(req, res) {
+  
+  getAppIdentityToken().then( j => callBackend(j, res) );
+});
+
 
 app.listen(app.get('port'), app.get('ip'), function() {
 
-
     console.log("Node app is running at localhost:" + app.get('port'))
-  })
+})
+
+
+function callBackend(accessToken, res) {
+
+  console.log('accessToken', accessToken.accessToken);
+ 
+  httpBackendResponses = ''; 
+  const req1 = httpRequest("http://127.0.0.1:8080/secured/date", accessToken.accessToken, 'Req1', 'secured/date');
+  const req2 = httpRequest("http://127.0.0.1:8080/secured/user", accessToken.accessToken, 'Req2', 'secured/user');
+  const req3 = httpRequest("http://127.0.0.1:8080/secured/dateForManager", accessToken.accessToken, 'Req3', 'secured/dateForManager');
+
+  Promise.all([req1, req2, req3])
+      .then(() => {
+
+        console.log('callBackend done.');
+        res.send(httpBackendResponses);
+      })
+  }
 
 module.exports = app;
